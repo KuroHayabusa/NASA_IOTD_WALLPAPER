2a4074407aa52db501da65a90a2880a3ffe9b28d
Josh Hall <Kuro.Hayabusa@gmail.com>
2016-01-03 00:49:22 +0000

    Added a function that takes an image URL and cache folder and  downloads the image to the cache folder in %AppData%

--- NASA_ITOD_WALLPAPER_svc/NASA_ITOD_WALLPAPER_svc/Service1.cs
+++ NASA_ITOD_WALLPAPER_svc/NASA_ITOD_WALLPAPER_svc/Service1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System;o
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,10 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Drawing;
+using System.IO;
+
 
 namespace NASA_ITOD_WALLPAPER_svc
 {
@@ -23,6 +27,29 @@ namespace NASA_ITOD_WALLPAPER_svc
 
         protected override void OnStop()
         {
+        }
+
+        private void GetRSSFeed(string URL)
+        {
+
+        }
+
+        private void CacheImage( string imageURL, string imageFileName, string cacheFolderName )
+        {
+
+            string cacheFilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            cacheFilePath = cacheFilePath + cacheFolderName + "\\" + imageFileName;
+
+            WebClient client = new WebClient();
+            
+            //download the file to the calculated cache );
+            client.DownloadFile(imageURL, cacheFilePath);
+                           
+        }
+
+        private void SetImageAsWallPaper()
+        {
+
         }
     }
 }
